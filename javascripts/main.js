54637ebb32b44fb85b93b124b94dd420f8c6507f
András Fülöp <fulibacsi.at.gmail.com>
2014-01-26 15:19:42 +0100

    Updated.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+jQuery(document).ready(function(){
+	jQuery('#main_content').find('div:not(.active)').hide();
+	jQuery('#content_triggers').find('span').click(function(){
+		jQuery('.active')
+			.hide()
+			.removeClass('active');
+		jQuery('#main_content')
+			.find('div.' + jQuery(this).attr('id'))
+			.addClass('active')
+			.show();
+	});
+});
